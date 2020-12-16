3fb4c0df9fa6e6e199e6821090213ac9ee11af46
Marcin Banaś <mb056625@student.ath.edu.pl>
2020-12-16 20:27:48 +0100

    Dodaj pliki projektów.

---
+++ Kolokwium1poprawione/Kontakt.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kolokwium
+{
+    class Kontakt
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int NrTelefonu { get; set; }
+        public DateTime DataUrodzenia { get; set; }
+
+        public Kontakt(string imie, string nazwisko, int nrtelefonu, DateTime dataurodzenia)
+        {
+
+
+            if ((Char.IsUpper(imie, 0) == true && Char.IsUpper(nazwisko, 0) == true) && (Convert.ToString(nrtelefonu).Length == 9) && dataurodzenia < DateTime.Now)
+            {
+                Imie = imie;
+                Nazwisko = nazwisko;
+                NrTelefonu = nrtelefonu;
+                DataUrodzenia = dataurodzenia;
+            }
+            else
+            {
+                throw new ArgumentException("Bledne dane, imie oraz nazwisko powinno zaczynac sie z duzej litery, nr. tel. powinien miec 9 znakow a data ur. nie moze byc nowsza niz obecna");
+            }
+        }
+    }
+}
---
+++ Kolokwium1poprawione/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace kolokwium
+{
+
+    class Program
+    {
+        public static void Mechanizm()
+        {
+            DateTime suma = new DateTime(2020, 12, 12, 0, 0, 0);
+            int srednia;
+            string h, m, s;
+            int wyswietlenia = 0;
+            do
+            {
+
+
+                Console.Write("Podaj czas (h,m,s):");
+                h = Console.ReadLine();
+                m = Console.ReadLine();
+                s = Console.ReadLine();
+                suma = suma.AddHours(int.Parse(h));
+                suma = suma.AddMinutes(int.Parse(m));
+                suma = suma.AddSeconds(int.Parse(s));
+                Console.WriteLine("Ogladasz:" + suma.Hour + "h:" + suma.Minute + "m:" + suma.Second + "s");
+                wyswietlenia = wyswietlenia + 1; ;
+                srednia = (((suma.Hour * 60 + suma.Minute) * 60 + suma.Second) / wyswietlenia);
+                Console.WriteLine("Srednia czasu na filmik:" + ((srednia / 60) / 60) + "h;" + ((srednia / 60) % 60) + "m:" + (srednia % 60) + "s");
+                Console.WriteLine("Ilosc filmikow:" + wyswietlenia);
+            }
+            while (suma < new DateTime(2020, 12, 13, 0, 0, 0));
+            Console.WriteLine("Przekroczyles dozwolony czas 23:59:59");
+        }
+        public static void Tablica()
+        {
+            double[,] tab = new double[4, 10];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    tab[i, j] = 1.5 * i + j * j + 1;
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(tab[i, j]); ;
+                    Console.Write(" ");
+                }
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Srednia (x=0,y=3; x=2; y=2; x=3,y=9)=" + (tab[0, 3] + tab[2, 2] + tab[3, 9]) / 3);
+        }
+        static void Main(string[] args)
+        {
+            Tablica();
+            Kontakt Person = new Kontakt("Marcin", "Banas", 123456789, new DateTime(2020, 8, 2));
+            Mechanizm();
+        }
+
+
+    }
+}
